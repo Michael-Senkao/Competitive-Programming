ba8bb2745c1e3fe419b8b6674882dde83f9aa648
Michael Senkao <senkaomichael@gmail.com>
2024-04-09 13:19:48 +0530

    Solve Tuples practice problem no. 1

---
+++ HackerRank/Tuples.py
@@ -0,0 +1,13 @@
+''' 
+QUESTION:
+Given an integer, , and  space-separated integers as input, create a tuple, , of those  integers. 
+Then compute and print the result of .
+
+Note: hash() is one of the functions in the __builtins__ module, so it need not be imported.
+'''
+
+if __name__ == '__main__':
+    n = int(input())
+    t = tuple(map(int, input().split()))
+
+    print(hash(t))
