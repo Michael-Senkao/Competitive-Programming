a8fb54d94b9891d52a53832cfb35f8f2efd7629f
Michael Senkao <senkaomichael@gmail.com>
2024-05-10 20:11:34 +0530

    Solve Car Pooling

---
+++ LeetCode/1094. Car Pooling/solution.py
@@ -0,0 +1,18 @@
+class Solution:
+    def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
+        n = len(trips)
+        range_sum = [0]*(1001)
+        
+        for trip in trips:
+            range_sum[trip[1]] += trip[0]
+            range_sum[trip[2]] -= trip[0]
+
+        if range_sum[0] > capacity:
+            return False
+
+        for i in range(1, 1001):
+            range_sum[i] += range_sum[i-1]
+            if range_sum[i] > capacity:
+                return False
+                    
+        return True
