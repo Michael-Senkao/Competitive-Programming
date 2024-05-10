8e591ba46c69cdbbe66ce9a8ea521a07ae4d043d
Michael Senkao <senkaomichael@gmail.com>
2024-05-10 20:46:51 +0530

    Update solution.py

--- LeetCode/1094. Car Pooling/solution.py
+++ LeetCode/1094. Car Pooling/solution.py
@@ -1,6 +1,5 @@
 class Solution:
     def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
-        n = len(trips)
         range_sum = [0]*(1001)
         
         for trip in trips:
