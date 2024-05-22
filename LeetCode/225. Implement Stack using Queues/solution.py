3fc67fdcb6f9f3f0f04b01f7d3c984e93e25b7ad
Michael Senkao <senkaomichael@gmail.com>
2024-05-22 23:31:04 +0530

    Initial Commit

---
+++ LeetCode/225. Implement Stack using Queues/solution.py
@@ -0,0 +1,40 @@
+class MyStack:
+
+    def __init__(self):
+        self.stack = deque()
+
+    def push(self, x: int) -> None:
+        self.stack.append(x)
+
+    def pop(self) -> int:
+        temp = deque()
+        while len(self.stack) > 1:
+            temp.append(self.stack.popleft())
+        
+        num = self.stack.popleft()
+        self.stack = temp
+
+        return num
+
+        
+
+    def top(self) -> int:
+        temp = deque()
+        while len(self.stack) > 1:
+            temp.append(self.stack.popleft())
+        
+        num = self.stack.popleft()
+        temp.append(num)
+        self.stack = temp
+        return num
+
+    def empty(self) -> bool:
+        return len(self.stack) == 0
+
+
+# Your MyStack object will be instantiated and called as such:
+# obj = MyStack()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.empty()
