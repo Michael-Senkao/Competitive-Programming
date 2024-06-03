5433af7ae6029b87cffb35827a58fb54b46b92b5
Michael Senkao <senkaomichael@gmail.com>
2024-06-03 20:23:14 +0530

    Initial Commit

--- GeeksforGeeks/Find duplicates in an array/solution.py
+++ GeeksforGeeks/Find duplicates in an array/solution.py
@@ -1,1 +1,12 @@
-
+class Solution:
+    def duplicates(self, arr, n): 
+	   
+	   res = []
+	   for num in arr:
+	       num %= n # Get the actual value at current index
+	       arr[num] += n # Increment the value at index 'num' by 'n'
+	   
+	   for i in range(n):
+	       if arr[i]//n > 1: 
+	           res.append(i) # Frequency of arr[i] is greater than 1
+	   return res if res else [-1]
