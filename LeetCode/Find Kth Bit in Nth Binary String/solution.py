22809da0a1b13095d9bab23daeddb66220c7d36c
Michael Senkao <senkaomichael@gmail.com>
2024-06-07 18:50:17 +0530

    Initial Commit

---
+++ LeetCode/Find Kth Bit in Nth Binary String/solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findKthBit(self, n: int, k: int) -> str:
+        if n==1:
+            return "0"
+        mid = 2**n/2
+        if k< mid:
+            return str(self.findKthBit(n-1,k)) # K is in lower half
+        elif k==mid:
+            return "1"
+        else:
+            return str(1- int(self.findKthBit(n-1,mid-(k-mid)))) # K is in upper half
