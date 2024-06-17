a32cc82ffb6d822b2b7c2994c91a0413b9fb256c
Michael Senkao <senkaomichael@gmail.com>
2024-06-17 23:28:20 +0530

    Time: 137 ms (20.77%) | Memory: 16.3 MB (96.81%) - LeetSync

--- LeetCode/633-sum-of-square-numbers/sum-of-square-numbers.py
+++ LeetCode/633-sum-of-square-numbers/sum-of-square-numbers.py
@@ -2,7 +2,7 @@ from math import sqrt
 class Solution:
     def judgeSquareSum(self, c: int) -> bool:
         start,end = 0, int(sqrt(c))
-        print(start, end)
+        
         while start <= end:
             if start**2+end**2 == c:
                 return True
