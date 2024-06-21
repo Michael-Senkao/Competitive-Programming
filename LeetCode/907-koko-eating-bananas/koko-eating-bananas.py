cb47a98126fcc3815c0c38bf3007d2898323bb7d
Michael Senkao <senkaomichael@gmail.com>
2024-06-21 22:24:15 +0530

    Time: 302 ms (21.26%) | Memory: 18.1 MB (80.17%) - LeetSync

--- LeetCode/907-koko-eating-bananas/koko-eating-bananas.py
+++ LeetCode/907-koko-eating-bananas/koko-eating-bananas.py
@@ -4,7 +4,7 @@ class Solution:
         def can_finish(k):
             i = 0
             count = 0
-            while i < n and count <= h:
+            while i < n:
                 count += ceil(piles[i]/k)
                 i += 1
             return count <= h
