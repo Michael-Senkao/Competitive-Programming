381809596eb3c9d61fa544b345e9e39595020998
Michael Senkao <senkaomichael@gmail.com>
2024-06-21 12:21:21 +0530

    Time: 230 ms (18.58%) | Memory: 19.1 MB (14.93%) - LeetSync

--- LeetCode/1138-grumpy-bookstore-owner/grumpy-bookstore-owner.py
+++ LeetCode/1138-grumpy-bookstore-owner/grumpy-bookstore-owner.py
@@ -1,19 +1,16 @@
 class Solution:
     def maxSatisfied(self, customers: List[int], grumpy: List[int], minutes: int) -> int:
         n=len(customers)
-        total_unsatisfied = 0
-        satisfied = 0
-        for i in range(n):
-            total_unsatisfied += customers[i] if grumpy[i]==1 else 0
-            satisfied += customers[i] if grumpy[i]==0 else 0
         
+        satisfied = 0
         window_sum = 0
         max_unsatisfied = 0
         left,right = 0,0
 
         while right < n:
             window_sum += customers[right] if grumpy[right]==1 else 0
-            
+            satisfied += customers[right] if grumpy[right]==0 else 0
+
             while right - left >= minutes:
                 if grumpy[left]==1:
                     window_sum -= customers[left]
