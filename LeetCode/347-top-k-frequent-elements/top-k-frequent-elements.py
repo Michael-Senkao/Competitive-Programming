866a1467c269db05a0630e1a33ade151420796d7
Michael Senkao <senkaomichael@gmail.com>
2024-06-27 22:10:49 +0530

    Time: 98 ms (17.14%) | Memory: 21 MB (79.14%) - LeetSync

--- LeetCode/347-top-k-frequent-elements/top-k-frequent-elements.py
+++ LeetCode/347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,7 +1,9 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         count = Counter(nums)
-        count = [(value, key) for key,value in count.items()]
-        count.sort(reverse=True)
-        res = [key for value, key in count[:k]]
+        count = [(-value, key) for key,value in count.items()]
+        res = []
+        heapify(count)
+        for i in range(k):
+            res.append(heappop(count)[1])
         return res
