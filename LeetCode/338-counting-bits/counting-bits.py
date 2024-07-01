3b0b4d1532dbd3a23925561e43156c1b28afa6f8
Michael Senkao <senkaomichael@gmail.com>
2024-07-01 21:01:46 +0530

    Time: 68 ms (44.61%) | Memory: 23.2 MB (66.03%) - LeetSync

--- LeetCode/338-counting-bits/counting-bits.py
+++ LeetCode/338-counting-bits/counting-bits.py
@@ -2,12 +2,12 @@ class Solution:
     def countBits(self, n: int) -> List[int]:
         def count_set_bit(num):
             count = 0
-            while num > 0:
+            while num >= len(res):
                 count += num&1
                 num >>= 1
-            return count
-        res = []
-        i = 0
+            return count + res[num]
+        res = [0]
+        i = 1
         while i <= n:
             res.append(count_set_bit(i))
             i += 1
