42318a3333ab5fad30c8480d6a83ec09dfbcefb4
Michael Senkao <senkaomichael@gmail.com>
2024-07-01 21:49:14 +0530

    Time: 101 ms (93.81%) | Memory: 17.9 MB (21.90%) - LeetSync

--- LeetCode/268-missing-number/missing-number.py
+++ LeetCode/268-missing-number/missing-number.py
@@ -1,17 +1,12 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        def cyclic_sort(nums):
-            i = 0
-            n = len(nums)
-            for i in range(n):
-                index = nums[i]
-                while index != i and index < n:
-                    nums[i],nums[index] = nums[index],nums[i]
-                    index = nums[i]
-
         n = len(nums)
-        cyclic_sort(nums)
-        for i in range(n):
-            if nums[i] != i:
-                return i
-        return n
+        res = 0
+
+        for i in range(n+1):
+            res ^= i
+
+        for num in nums:
+            res ^= num
+            
+        return res
