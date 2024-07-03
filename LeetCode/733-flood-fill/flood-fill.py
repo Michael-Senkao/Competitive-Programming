0e03617db2eaac39d65ac3070417512de85c27de
Michael Senkao <senkaomichael@gmail.com>
2024-07-03 22:19:15 +0530

    Time: 71 ms (29.26%) | Memory: 16.6 MB (59.00%) - LeetSync

---
+++ LeetCode/733-flood-fill/flood-fill.py
@@ -0,0 +1,22 @@
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        def dfs(r, c):
+            if 0 <= r < m and 0 <= c < n and image[r][c] == prevColor and (r,c) not in visited:
+                visited.add((r,c))
+                image[r][c] = color
+                dfs(r+1, c)
+                dfs(r-1, c)
+                dfs(r, c + 1)
+                dfs(r, c - 1)
+                
+        
+        m = len(image)
+        n = len(image[0])
+        prevColor = image[sr][sc]
+        visited = set()
+        
+
+        dfs(sr, sc)
+
+        return image
+        
