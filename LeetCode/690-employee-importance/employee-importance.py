660c33ddad814dbcfebc8df68e3fbe08bcc8369d
Michael Senkao <senkaomichael@gmail.com>
2024-07-04 19:05:34 +0530

    Time: 122 ms (20.40%) | Memory: 18 MB (14.15%) - LeetSync

---
+++ LeetCode/690-employee-importance/employee-importance.py
@@ -0,0 +1,23 @@
+"""
+# Definition for Employee.
+class Employee:
+    def __init__(self, id: int, importance: int, subordinates: List[int]):
+        self.id = id
+        self.importance = importance
+        self.subordinates = subordinates
+"""
+
+class Solution:
+    def getImportance(self, employees: List['Employee'], id: int) -> int:
+        def dfs(e_id):
+            sum = 0
+            for subordinate in graph[e_id][1:]:
+                sum += dfs(subordinate)
+            return sum + graph[e_id][0]
+
+
+        graph = defaultdict(list)
+        for employee in employees:
+            graph[employee.id] = [employee.importance] + employee.subordinates
+
+        return dfs(id)
