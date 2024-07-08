6b46da2e910058b6d07ecb79c08b1485f37b9338
Michael Senkao <senkaomichael@gmail.com>
2024-07-08 20:05:53 +0530

    Time: 79 ms (23.57%) | Memory: 18.2 MB (27.63%) - LeetSync

--- LeetCode/813-all-paths-from-source-to-target/all-paths-from-source-to-target.py
+++ LeetCode/813-all-paths-from-source-to-target/all-paths-from-source-to-target.py
@@ -1,8 +1,6 @@
 class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
         
-        # adjList = defaultdict(list)
-        # for i in range(len(graph)):
         res = []
         n = len(graph)
 
