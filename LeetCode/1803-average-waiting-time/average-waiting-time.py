a50e996aee699c94c4324be3546d0a9348ea341e
Michael Senkao <senkaomichael@gmail.com>
2024-07-09 12:25:51 +0530

    Time: 788 ms (15.12%) | Memory: 56.9 MB (95.53%) - LeetSync

--- LeetCode/1803-average-waiting-time/average-waiting-time.py
+++ LeetCode/1803-average-waiting-time/average-waiting-time.py
@@ -3,9 +3,11 @@ class Solution:
         n = len(customers)
         total_waiting_time = 0
         current_time = 0
-        for i in range(n):
-            start_time = max(current_time, customers[i][0])
-            completion_time = start_time + customers[i][1]
-            total_waiting_time += completion_time - customers[i][0]
+
+        for customer in customers:
+            start_time = max(current_time, customer[0])
+            completion_time = start_time + customer[1]
+            total_waiting_time += completion_time - customer[0]
             current_time = completion_time
+
         return total_waiting_time / n
