6ae1f0c5c5ab85476f897678f628465a77052b1a
Michael Senkao <senkaomichael@gmail.com>
2024-07-09 11:57:22 +0530

    Time: 653 ms (15.87%) | Memory: 293.2 MB (5.07%) - LeetSync

--- LeetCode/518-coin-change-ii/coin-change-ii.py
+++ LeetCode/518-coin-change-ii/coin-change-ii.py
@@ -1,13 +1,13 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
         @cache
-        def backtrack(index, curr_sum):
+        def dp(index, curr_sum):
             if curr_sum == amount:
                 return 1
             if index >= n or curr_sum > amount:
                 return 0
             
-            return backtrack(index, curr_sum + coins[index]) + backtrack(index+1, curr_sum)
+            return dp(index, curr_sum + coins[index]) + dp(index+1, curr_sum)
 
         n = len(coins)
-        return backtrack(0, 0)
+        return dp(0, 0)
