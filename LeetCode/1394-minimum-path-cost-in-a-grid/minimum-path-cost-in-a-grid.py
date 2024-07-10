97162e0278a445ecd2c595471b695050bb0f1c95
Michael Senkao <senkaomichael@gmail.com>
2024-07-10 22:09:50 +0530

    Time: 1761 ms (26.03%) | Memory: 22.5 MB (36.07%) - LeetSync

--- LeetCode/1394-minimum-path-cost-in-a-grid/minimum-path-cost-in-a-grid.py
+++ LeetCode/1394-minimum-path-cost-in-a-grid/minimum-path-cost-in-a-grid.py
@@ -4,17 +4,15 @@ class Solution:
         dp = [[0 for _ in range(m)] for _ in range(n)]
         
         
-        print(dp)
-        for i in range(n-1):
+        for i in range(n):
             for j in range(m):
                 dp[i][j] += grid[i][j]
                 for k in range(m):
-                    if dp[i+1][k] == 0:
-                        dp[i+1][k] = dp[i][j] + moveCost[grid[i][j]][k]
-                    else:
-                        dp[i+1][k] = min(dp[i+1][k], dp[i][j] + moveCost[grid[i][j]][k])
+                    if i + 1 < n:
+                        if dp[i+1][k] == 0:
+                            dp[i+1][k] = dp[i][j] + moveCost[grid[i][j]][k]
+                        else:
+                            dp[i+1][k] = min(dp[i+1][k], dp[i][j] + moveCost[grid[i][j]][k])
         
-        res = float("inf")
-        for i in range(m):
-            res = min(res, dp[n-1][i] + grid[n-1][i])
-        return res
+        
+        return min(dp[-1])
