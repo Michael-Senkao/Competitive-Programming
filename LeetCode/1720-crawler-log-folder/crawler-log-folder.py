3c1bf8cb2cd25e7e8066adf963114c2df0334c29
Michael Senkao <senkaomichael@gmail.com>
2024-07-10 19:27:19 +0530

    Time: 56 ms (13.93%) | Memory: 16.7 MB (36.19%) - LeetSync

---
+++ LeetCode/1720-crawler-log-folder/crawler-log-folder.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minOperations(self, logs: List[str]) -> int:
+        n = len(logs)
+        stack = []
+
+        for log in logs:
+            if log == "../":
+                if stack:
+                    stack.pop()
+            elif log != "./":
+                stack.append(log)
+        print(stack)
+        return len(stack)
