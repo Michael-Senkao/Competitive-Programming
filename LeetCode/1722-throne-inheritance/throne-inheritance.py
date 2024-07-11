76e55e360bf717ec06b45ca43e83326276de71ae
Michael Senkao <senkaomichael@gmail.com>
2024-07-11 20:40:22 +0530

    Time: 630 ms (73.97%) | Memory: 69.9 MB (86.98%) - LeetSync

---
+++ LeetCode/1722-throne-inheritance/throne-inheritance.py
@@ -0,0 +1,38 @@
+class ThroneInheritance:
+
+    def __init__(self, kingName: str):
+        self.root = kingName
+        self.family = {kingName: []}
+        self.dead = set()
+
+    def birth(self, parentName: str, childName: str) -> None:
+        self.family[parentName].append(childName)
+        self.family[childName] = []
+
+    def death(self, name: str) -> None:
+        self.dead.add(name)
+
+    def getInheritanceOrder(self) -> List[str]:
+        return self.dfs(self.root)
+
+    def dfs(self, node):
+        stack = [node]
+        res = []
+
+        while stack:
+            curr = stack.pop()
+            if curr not in self.dead:
+                res.append(curr)
+
+            for child in self.family[curr][::-1]:
+                stack.append(child)
+
+        return res
+        
+
+
+# Your ThroneInheritance object will be instantiated and called as such:
+# obj = ThroneInheritance(kingName)
+# obj.birth(parentName,childName)
+# obj.death(name)
+# param_3 = obj.getInheritanceOrder()
