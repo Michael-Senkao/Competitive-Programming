f05096d166b9defcc8c6f5cd2b83a61163f11185
Michael Senkao <senkaomichael@gmail.com>
2024-07-15 12:05:11 +0530

    Time: 1565 ms (64.65%) | Memory: 25.8 MB (67.68%) - LeetSync

--- LeetCode/2306-create-binary-tree-from-descriptions/create-binary-tree-from-descriptions.py
+++ LeetCode/2306-create-binary-tree-from-descriptions/create-binary-tree-from-descriptions.py
@@ -6,31 +6,35 @@
 #         self.right = right
 class Solution:
     def createBinaryTree(self, descriptions: List[List[int]]) -> Optional[TreeNode]:
-        def dfs(val):
-            parent = TreeNode(val)
-            for child in adj[val]:
-                if child[1]==1:
-                    parent.left = dfs(child[0])
-                else:
-                    parent.right = dfs(child[0])
-
+        def makeNode(parent, child, position):
+            if position == 1:
+                parent.left = child
+            else:
+                parent.right = child
             return parent
 
-        def findRoot():
-            for parent in adj.keys():
-                if parent not in children:
-                    return parent
-
-
-        
-        adj = defaultdict(list)
+        adj = defaultdict(TreeNode)
         children = set()
         
         for parent, child, position in descriptions:
-            adj[parent].append((child, position))
+            if parent in adj and child in adj:
+                adj[parent] = makeNode(adj[parent], adj[child], position)
+            elif parent in adj:
+                childNode = TreeNode(child) 
+                adj[parent] = makeNode(adj[parent], childNode, position)
+                adj[child] = childNode
+            elif child in adj:
+                parNode = TreeNode(parent)
+                adj[parent] = makeNode(parNode, adj[child], position) 
+            else:
+                childNode = TreeNode(child)
+                adj[parent] = makeNode(TreeNode(parent), childNode, position)
+                adj[child] = childNode
+
             children.add(child)
 
-        root = findRoot()
-        # print(adj)
-        # print(root)
-        return dfs(root)
+
+        for val,node in adj.items():
+            if val not in children:
+                return node
+    
