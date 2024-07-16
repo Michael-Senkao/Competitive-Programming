9b6b4ee4dcd7d5435d2bb47726f248a74539e52e
Michael Senkao <senkaomichael@gmail.com>
2024-07-17 00:14:20 +0530

    Time: 1094 ms (12.73%) | Memory: 18.8 MB (27.27%) - LeetSync

---
+++ LeetCode/2104-operations-on-tree/operations-on-tree.py
@@ -0,0 +1,62 @@
+class LockingTree:
+
+    def __init__(self, parent: List[int]):
+        self.parents = defaultdict(int)
+        self.descendants = defaultdict(list)
+        self.locked = defaultdict(int)
+
+        for i in range(len(parent)):
+            self.parents[i] = parent[i]
+            self.descendants[parent[i]].append(i)
+
+        del self.descendants[-1]
+        
+
+
+    def lock(self, num: int, user: int) -> bool:
+        if num not in self.locked:
+            self.locked[num] = user
+            return True
+        return False
+
+    def unlock(self, num: int, user: int) -> bool:
+        if num in self.locked and self.locked[num] == user:
+            del self.locked[num]
+            return True
+        return False
+
+    def upgrade(self, num: int, user: int) -> bool:
+        if num in self.locked:
+            return False
+        descendents = set()
+        q = deque([num])
+        flag = False
+        while q:
+            curr = q.popleft()
+            for child in self.descendants[curr]:
+                if child in self.locked:
+                    flag = True
+                descendents.add(child)
+                q.append(child)
+       
+        if not flag:
+            return False
+
+        parent = self.parents[num]
+        while parent != -1 and parent not in self.locked:
+            parent = self.parents[parent]
+        if parent != -1:
+            return False
+        
+        for child in descendents:
+            if child in self.locked:
+                del self.locked[child]
+        self.locked[num] = user
+        return True
+
+
+# Your LockingTree object will be instantiated and called as such:
+# obj = LockingTree(parent)
+# param_1 = obj.lock(num,user)
+# param_2 = obj.unlock(num,user)
+# param_3 = obj.upgrade(num,user)
