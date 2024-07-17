03e32204cfc4788fe3e432400102df4856d0c140
Michael Senkao <senkaomichael@gmail.com>
2024-07-17 19:34:40 +0530

    Time: 60 ms (84.18%) | Memory: 17 MB (36.84%) - LeetSync

--- LeetCode/871-keys-and-rooms/keys-and-rooms.py
+++ LeetCode/871-keys-and-rooms/keys-and-rooms.py
@@ -3,14 +3,12 @@ class Solution:
         
         q = deque([0])
         visited = set([0])
-        
-
+    
         while q:
             curr = q.popleft()
             for room in rooms[curr]:
                 if room not in visited:
                     visited.add(room)
                     q.append(room)
-
 
         return len(visited) == len(rooms)
