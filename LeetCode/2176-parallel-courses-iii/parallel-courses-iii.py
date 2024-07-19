84faf67378d9ea0e18279e8f25d2f1cdae3a6286
Michael Senkao <senkaomichael@gmail.com>
2024-07-19 20:39:00 +0530

    Time: 1288 ms (53.99%) | Memory: 45.8 MB (61.30%) - LeetSync

--- LeetCode/2176-parallel-courses-iii/parallel-courses-iii.py
+++ LeetCode/2176-parallel-courses-iii/parallel-courses-iii.py
@@ -3,7 +3,8 @@ class Solution:
         
         adj = defaultdict(list)
         indegree = defaultdict(int)
-        ans = [0 for _ in range(n)]
+        finish_time = [0 for _ in range(n)]
+
         for prev,next_ in relations:
             adj[prev].append(next_)
             indegree[next_] += 1
@@ -12,21 +13,19 @@ class Solution:
         for i in range(1,n+1):
             if indegree[i] == 0:
                 q.append(i)
-        # print(adj)
-        # print(indegree)
         
         while q:
-            # print(q)
+    
             curr = q.popleft()
-            ans[curr-1] += time[curr-1]
+            finish_time[curr-1] += time[curr-1]
             for nei in adj[curr]:
-                if ans[nei - 1] == 0:
-                    ans[nei - 1] = ans[curr-1]
+                if finish_time[nei - 1] == 0:
+                    finish_time[nei - 1] = finish_time[curr-1]
                 else:
-                    ans[nei - 1] = max(ans[nei - 1], ans[curr - 1])
+                    finish_time[nei - 1] = max(finish_time[nei - 1], finish_time[curr - 1])
                 
                 indegree[nei] -= 1
                 if indegree[nei] == 0:
                     q.append(nei)
-        # print(ans)
-        return max(ans)
+        
+        return max(finish_time)
