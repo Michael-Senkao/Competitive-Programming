bbdc9641e06cdd7599403ae7680d2fadde7e57fc
Michael Senkao <senkaomichael@gmail.com>
2024-07-20 19:41:12 +0530

    Time: 692 ms (28.11%) | Memory: 18.1 MB (44.26%) - LeetSync

--- LeetCode/2038-nearest-exit-from-entrance-in-maze/nearest-exit-from-entrance-in-maze.py
+++ LeetCode/2038-nearest-exit-from-entrance-in-maze/nearest-exit-from-entrance-in-maze.py
@@ -3,16 +3,7 @@ class Solution:
         def bfs(i,j):
             def valid(r,c):
                 return r>=0 and c>=0 and r < rows and c<cols and maze[r][c] != "+" and (r,c) not in visited
-            q = deque()
-            visited = set()
-            q.append((i,j,0))
-            visited.add((i,j))
-            while q:
-                row,col,cost = q.popleft()
-                
-                if (row == 0 or col == 0 or row == rows-1 or col == cols-1):
-                    if (row,col) != (i,j):
-                        return cost
+            def getValidIndexes(row, col, cost):
                 if valid(row, col+1):
                     q.append((row,col+1, cost + 1))
                     visited.add((row,col + 1))
@@ -26,6 +17,18 @@ class Solution:
                 if valid(row-1, col):
                     q.append((row-1,col, cost + 1))
                     visited.add((row - 1,col))
+        
+            q = deque()
+            visited = set()
+            getValidIndexes(i, j, 0)
+            visited.add((i,j))
+
+            while q:
+                row,col,cost = q.popleft()
+                
+                if (row == 0 or col == 0 or row == rows-1 or col == cols-1):
+                    return cost
+                getValidIndexes(row, col, cost)
 
             return -1
         rows,cols = len(maze),len(maze[0])
