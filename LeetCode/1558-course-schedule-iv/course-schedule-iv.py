365a0c214935eaa2f4c7bb0da8c867269aa39a98
Michael Senkao <senkaomichael@gmail.com>
2024-07-21 19:03:09 +0530

    Time: 756 ms (29.74%) | Memory: 20.4 MB (21.52%) - LeetSync

---
+++ LeetCode/1558-course-schedule-iv/course-schedule-iv.py
@@ -0,0 +1,31 @@
+class Solution:
+    def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
+
+        indegree = [0 for _ in range(numCourses)]
+        graph = defaultdict(list)
+
+        for u,v in prerequisites:
+            graph[u].append(v)
+            indegree[v] += 1
+        print(graph)
+        print(indegree)
+        q = deque()
+        pre = defaultdict(set)
+
+        for i in range(numCourses):
+            if indegree[i] == 0:
+                q.append(i)
+
+        while q:
+            curr = q.popleft()
+            for nei in graph[curr]:
+                pre[nei].add(curr)
+                pre[nei].update(pre[curr])
+                print(pre[nei], pre[curr])
+                indegree[nei] -= 1
+                if indegree[nei] == 0:
+                    q.append(nei)
+        res = [u in pre[v] for u,v in queries]
+        print(pre)
+        return res
+                        
