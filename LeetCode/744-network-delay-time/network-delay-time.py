1eb5cb4137aa86e6a6d6efd214dc49e064016bfa
Michael Senkao <senkaomichael@gmail.com>
2024-07-23 00:32:54 +0530

    Time: 393 ms (21.86%) | Memory: 18.6 MB (27.80%) - LeetSync

---
+++ LeetCode/744-network-delay-time/network-delay-time.py
@@ -0,0 +1,35 @@
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        graph = defaultdict(list)
+        costs = {node: float("inf") for node in range(1, n+1)}
+        indegree = {node: 0 for node in range(1, n+1)}
+
+        for u,v,w in times:
+            graph[u].append((v,w))
+            indegree[v] += 1
+
+        visited = set()
+        q = deque([k])
+        visited.add(k)
+        costs[k] = 0
+        
+
+        while q:
+            current_node = q.popleft()
+            for neighbor, cost in graph[current_node]:
+                if neighbor in visited and costs[current_node] + cost < costs[neighbor]:
+                    costs[neighbor] =  costs[current_node] + cost
+                    q.append(neighbor)
+                elif neighbor not in visited:
+                    costs[neighbor] = costs[current_node] + cost
+                    q.append(neighbor)
+                    visited.add(neighbor)
+
+        print(costs)
+        print(visited)
+        res = 0
+        for c in costs.values():
+            if c == float("inf"):
+                return -1
+            res = max(res, c)
+        return res
