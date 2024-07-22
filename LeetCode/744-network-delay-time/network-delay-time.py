eea26667b3f6bd133f967a7363b56566eb9998d6
Michael Senkao <senkaomichael@gmail.com>
2024-07-23 00:56:40 +0530

    Time: 389 ms (25.04%) | Memory: 18.6 MB (54.87%) - LeetSync

--- LeetCode/744-network-delay-time/network-delay-time.py
+++ LeetCode/744-network-delay-time/network-delay-time.py
@@ -2,21 +2,17 @@ class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
         graph = defaultdict(list)
         costs = {node: float("inf") for node in range(1, n+1)}
-        indegree = {node: 0 for node in range(1, n+1)}
-
+        
         for u,v,w in times:
             graph[u].append((v,w))
-            indegree[v] += 1
 
         visited = set()
         q = [(0, k)]
         heapify(q)
-        # visited.add(k)
         costs[k] = 0
         
 
         while q:
-            # print(q)
             curr_cost, current_node = heappop(q)
             if current_node in visited:
                 continue
