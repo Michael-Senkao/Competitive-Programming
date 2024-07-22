76360f9e04f0a7889bd82fba4f8e13dec5f82c56
Michael Senkao <senkaomichael@gmail.com>
2024-07-22 14:05:42 +0530

    Time: 103 ms (65.90%) | Memory: 17.1 MB (14.55%) - LeetSync

--- LeetCode/2502-sort-the-people/sort-the-people.py
+++ LeetCode/2502-sort-the-people/sort-the-people.py
@@ -2,5 +2,5 @@ class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
         names_index = [(index, name) for index,name in enumerate(names)]
         names_index.sort(key = lambda x: -heights[x[0]])
-        print(names_index)
+        # print(names_index)
         return [name for index,name in names_index]
