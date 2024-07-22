9c817848c2ea90bef4e8066412b8a7762ab1a27b
Michael Senkao <senkaomichael@gmail.com>
2024-07-23 00:59:48 +0530

    Time: 395 ms (20.34%) | Memory: 18.6 MB (54.87%) - LeetSync

--- LeetCode/744-network-delay-time/network-delay-time.py
+++ LeetCode/744-network-delay-time/network-delay-time.py
@@ -1,26 +1,28 @@
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
         graph = defaultdict(list)
-        costs = {node: float("inf") for node in range(1, n+1)}
+        costs = {node: float("inf") for node in range(1, n+1)} # Stores the cost to reach each node in the network
         
+        # Represent the network as a graph
         for u,v,w in times:
             graph[u].append((v,w))
 
         visited = set()
-        q = [(0, k)]
-        heapify(q)
+        heap = [(0, k)] #(cost, node)
+        heapify(heap)
         costs[k] = 0
         
 
-        while q:
-            curr_cost, current_node = heappop(q)
+        while heap:
+            current_cost, current_node = heappop(heap)
             if current_node in visited:
                 continue
-            costs[current_node] = curr_cost
+            costs[current_node] = current_cost
             visited.add(current_node) 
-            for neighbor, cost in graph[current_node]:
-                heappush(q, (curr_cost + cost, neighbor))
 
-        if len(visited) != n:
+            for neighbor, cost in graph[current_node]:
+                heappush(heap, (current_cost + cost, neighbor))
+
+        if len(visited) != n: # All nodes cannot be visited
             return -1
         return max(costs.values())
