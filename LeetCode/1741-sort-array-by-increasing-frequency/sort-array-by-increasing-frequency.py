9b8677fee4c62a5a099287ecd35f634c457cde48
Michael Senkao <senkaomichael@gmail.com>
2024-07-23 14:40:50 +0530

    Time: 50 ms (55.36%) | Memory: 16.5 MB (37.68%) - LeetSync

---
+++ LeetCode/1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.py
@@ -0,0 +1,7 @@
+class Solution:
+    def frequencySort(self, nums: List[int]) -> List[int]:
+        freq = Counter(nums)
+        print(freq)
+        nums.sort(reverse = True)
+        nums.sort(key = lambda num: freq[num])
+        return nums
