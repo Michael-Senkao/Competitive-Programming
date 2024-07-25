fd0805dfd96900ba4a57fa553fdde4a5a36d1b0b
Michael Senkao <senkaomichael@gmail.com>
2024-07-25 15:46:34 +0530

    Time: 1417 ms (11.88%) | Memory: 36.3 MB (7.43%) - LeetSync

--- LeetCode/948-sort-an-array/sort-an-array.py
+++ LeetCode/948-sort-an-array/sort-an-array.py
@@ -1,25 +1,34 @@
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
-        def mergeSort(left, right, arr):
-            if left < right:
-                mid = (left + right)//2
-                return merge(mergeSort(left, mid, arr), mergeSort(mid + 1, right, arr))
-            return [arr[left]]
-        
-        def merge(left, right):
-            n,m = len(left),len(right)
-            i,j = 0,0
-            res = []
-            while i < n and j < m:
-                if right[j] < left[i]:
-                    res.append(right[j])
-                    j += 1
-                else:
-                    res.append(left[i])
-                    i += 1
+        def heap_sort(nums):
+            for i in range(len(nums)):
+                heapify_up(i)
             
-            res.extend(left[i:])
-            res.extend(right[j:])
-            return res
+            for i in range(len(nums) - 1, 0, -1):
+                nums[i],nums[0] = nums[0],nums[i]
+                heapify_down(0, i - 1)
 
-        return mergeSort(0, len(nums)-1, nums)
+                
+        def heapify_up(index):
+            if index > 0:
+                parent = (index-1)//2
+                if nums[parent] < nums[index]:
+                    nums[parent],nums[index] = nums[index],nums[parent]
+                    heapify_up(parent)
+
+        def heapify_down(index, n):
+            max_index = index
+            left = 2*index + 1
+            right = left + 1
+            if left <= n and nums[left] > nums[max_index]:
+                max_index = left
+            if right <= n and nums[right] > nums[max_index]:
+                max_index = right
+            if max_index != index:
+                nums[index],nums[max_index] = nums[max_index],nums[index]
+                heapify_down(max_index, n)
+
+        
+        # print(nums)
+        heap_sort(nums)
+        return nums
