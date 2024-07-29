c631bd3ecf167eeb42eb5552d2292476378b419c
Michael Senkao <senkaomichael@gmail.com>
2024-07-29 20:30:44 +0530

    Time: 60 ms (43.16%) | Memory: 16.8 MB (6.59%) - LeetSync

---
+++ LeetCode/692-top-k-frequent-words/top-k-frequent-words.py
@@ -0,0 +1,11 @@
+class Solution:
+    def topKFrequent(self, words: List[str], k: int) -> List[str]:
+        heap = [(-frequency, word) for word,frequency in Counter(words).items()]
+        heapify(heap)
+        ans = []
+
+        while k and heap:
+            freq,word = heappop(heap)
+            ans.append(word)
+            k -= 1
+        return ans
