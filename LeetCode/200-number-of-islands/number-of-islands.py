578c4c01dc4432fd0f11098bdee5ca49c9fefc6d
Michael Senkao <senkaomichael@gmail.com>
2024-08-09 19:55:33 +0530

    Time: 417 ms (5.07%) | Memory: 40.5 MB (5.56%) - LeetSync

---
+++ LeetCode/200-number-of-islands/number-of-islands.py
@@ -0,0 +1,64 @@
+class UnionFind:
+    def __init__(self, rows, cols, grid):
+        self.parent = {}
+        self.size = [[0 for _ in range(cols)] for _ in range(rows)]
+
+        for i in range(rows):
+            for j in range(cols):
+                self.parent[(i,j)] = (i, j)
+                self.size[i][j] = grid[i][j]
+    
+    def find(self, x: tuple):
+        if x == self.parent[x]:
+            return x
+        self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+    
+    def union(self, x:tuple, y:tuple):
+        r1,c1 = self.find(x)
+        r2,c2 = self.find(y)
+        
+        if (r1,c1) != (r2,c2):
+            if self.size[r1][c1] > self.size[r2][c2]:
+                self.parent[(r2,c2)] = (r1,c1)
+                self.size[r1][c1] += self.size[r2][c2]
+            else:
+                self.parent[(r1,c1)] = (r2,c2)
+                self.size[r2][c2] += self.size[r1][c1]
+
+
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+
+        def connect(i,j):
+            if valid(i, j + 1):
+                dsu.union((i,j), (i, j + 1))
+            if valid(i, j - 1):
+                dsu.union((i,j), (i, j - 1))
+            if valid(i + 1, j):
+                dsu.union((i,j), (i + 1, j))
+            if valid(i - 1, j):
+                dsu.union((i,j), (i - 1, j))
+
+        def valid(r,c):
+            if r < 0 or c < 0 or r >= rows or c >= cols or grid[r][c] == "0":
+                return False
+            return True
+
+        
+        rows,cols = len(grid), len(grid[0])
+        dsu = UnionFind(rows, cols, grid)
+        islands = set()
+
+        for i in range(rows):
+            for j in range(cols):
+                if valid(i,j):
+                    connect(i,j)
+                    
+        # Find the number of distinct representatives
+        for i in range(rows):
+            for j in range(cols):
+                if valid(i,j):
+                    islands.add(dsu.find((i,j)))
+        
+        return len(islands)
