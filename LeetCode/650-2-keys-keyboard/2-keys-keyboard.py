41f2a385826283ef3805c07b4a719c8a4c90c6de
Michael Senkao <senkaomichael@gmail.com>
2024-08-19 18:46:17 +0530

    Time: 30 ms (95.56%) | Memory: 16.6 MB (36.56%) - LeetSync

--- LeetCode/650-2-keys-keyboard/2-keys-keyboard.py
+++ LeetCode/650-2-keys-keyboard/2-keys-keyboard.py
@@ -1,7 +1,21 @@
 class Solution:
     def minSteps(self, n: int) -> int:
+        def is_prime(n):
+            if n <= 1:
+                return False
+            if n == 2:
+                return True
+            if n % 2 == 0:
+                return False
+            for i in range(3, int(n**0.5) + 1, 2):
+                if n % i == 0:
+                    return False
+            return True
+
         if n == 1:
             return 0
+        if is_prime(n):
+            return n
 
         cache = 1
         answer = 1
