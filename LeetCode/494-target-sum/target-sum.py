6db3e1190c90ca397c5181a617b220da518de852
Michael Senkao <senkaomichael@gmail.com>
2024-08-21 19:42:09 +0530

    Time: 225 ms (27.58%) | Memory: 37.7 MB (47.36%) - LeetSync

---
+++ LeetCode/494-target-sum/target-sum.py
@@ -0,0 +1,21 @@
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        memo = {}
+        
+        def dp(index, curr_value):
+            if index == len(nums):
+                return 1 if curr_value == target else 0
+            
+            # Check if the result is already computed
+            if (index, curr_value) in memo:
+                return memo[(index, curr_value)]
+            
+            # Calculate the number of ways by adding and subtracting the current number
+            add = dp(index + 1, curr_value + nums[index])
+            subtract = dp(index + 1, curr_value - nums[index])
+            
+            # Store the result in the memo dictionary
+            memo[(index, curr_value)] = add + subtract
+            return memo[(index, curr_value)]
+        
+        return dp(0, 0)
