8b284a18ec55683aaa25f6228ac6cf41ed39107a
Michael Senkao <senkaomichael@gmail.com>
2024-08-25 13:51:15 +0530

    Time: 129 ms (40.32%) | Memory: 18 MB (94.27%) - LeetSync

--- LeetCode/283-move-zeroes/move-zeroes.py
+++ LeetCode/283-move-zeroes/move-zeroes.py
@@ -4,14 +4,12 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         n = len(nums)
-        non_zero = 0
+        left = right = 0
 
-        for index in range(n):
-            if nums[index] == 0:
-                while non_zero < n and (nums[non_zero] == 0 or non_zero < index):
-                    non_zero += 1
-                if non_zero < n:
-                    nums[index], nums[non_zero] = nums[non_zero],0
-                else:
-                    return
-        
+        while right < n:
+            if nums[right] != 0:
+                while left < right and nums[left] != 0:
+                    left += 1
+                nums[left],nums[right] = nums[right],nums[left]
+                left += 1
+            right += 1
