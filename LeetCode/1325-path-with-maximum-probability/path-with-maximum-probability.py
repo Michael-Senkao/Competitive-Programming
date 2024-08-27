7248c29fe350607d10c0042a1a029e6c404a8484
Michael Senkao <senkaomichael@gmail.com>
2024-08-27 13:34:30 +0530

    Time: 628 ms (13.04%) | Memory: 30 MB (8.22%) - LeetSync

---
+++ LeetCode/1325-path-with-maximum-probability/path-with-maximum-probability.py
@@ -0,0 +1,29 @@
+class Solution:
+    def maxProbability(self, n: int, edges: List[List[int]], succProb: List[float], start_node: int, end_node: int) -> float:
+
+        graph = defaultdict(list)
+        max_prob = [0]*n
+        max_prob[start_node] = 1
+        res = 0
+        for i in range(len(edges)):
+            u,v = edges[i]
+            graph[u].append((v, succProb[i]))
+            graph[v].append((u, succProb[i]))
+
+        q = deque([start_node])
+        visited = set([start_node])
+
+        while q:
+            curr = q.popleft()
+            for nei, prob in graph[curr]:
+                if max_prob[curr]*prob > max_prob[nei]:
+                    max_prob[nei] = max_prob[curr]*prob
+                    q.append(nei)
+                elif nei not in visited:
+                    visited.add(nei)
+                    q.append(nei)
+        
+        return max_prob[end_node]
+
+
+        
