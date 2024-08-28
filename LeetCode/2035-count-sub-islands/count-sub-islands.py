6815f8c7ef626d72bce1125ce618f8b854d31cc1
Michael Senkao <senkaomichael@gmail.com>
2024-08-28 11:43:15 +0530

    Time: 2349 ms (23.23%) | Memory: 70.3 MB (5.06%) - LeetSync

--- LeetCode/2035-count-sub-islands/count-sub-islands.py
+++ LeetCode/2035-count-sub-islands/count-sub-islands.py
@@ -1,30 +1,58 @@
 class Solution:
     def countSubIslands(self, grid1: List[List[int]], grid2: List[List[int]]) -> int:
-        def dfs(i,j):
-            if i < 0 or j < 0 or i >= rows or j >= cols or grid2[i][j]==0 or (i,j) in visited:
-                return True
-            
-            visited.add((i,j))
-            result = True
+        def bfs(start_i, start_j):
+            islands = [(start_i, start_j)]
+            q = deque([(start_i, start_j)])
+            visited2.add((start_i, start_j))
 
-            if grid1[i][j] == 0:
-                result = False
-            
-            result = dfs(i,j+1) and result
-            result = dfs(i, j-1) and result 
-            result = dfs(i+1, j) and result
-            result = dfs(i-1, j) and result
-            return result
-            
-            
+            while q:
+                r,c = q.popleft()
+                if (c + 1 < cols 
+                    and grid2[r][c+1] == 1 and (r, c + 1) not in visited2):
+                    visited2.add((r, c + 1))
+                    q.append((r, c + 1))
+                    islands.append((r, c + 1))
+                if (c - 1 >= 0
+                    and grid2[r][c - 1] == 1 and (r, c - 1) not in visited2):
+                    visited2.add((r, c - 1))
+                    q.append((r, c - 1))
+                    islands.append((r, c - 1))
+                if (r + 1 < rows 
+                    and grid2[r + 1][c] == 1 and (r + 1, c) not in visited2):
+                    visited2.add((r + 1, c))
+                    q.append((r + 1, c))
+                    islands.append((r + 1, c))
+                if (r - 1 >= 0 
+                    and grid2[r - 1][c] == 1 and (r - 1, c) not in visited2):
+                    visited2.add((r - 1, c))
+                    q.append((r - 1, c))
+                    islands.append((r - 1, c))
+
+            return islands
             
 
-        rows,cols = len(grid1), len(grid1[0])
-        visited = set()
+        rows, cols = len(grid1), len(grid1[0])
+        island_cells1 = set()
+        islands2 = []
+        visited2 = set()
+        directions = [1,-1]
         res = 0
 
-        for i in range(rows):
-            for j in range(cols):
-                if grid2[i][j]==1 and (i,j) not in visited and dfs(i,j):
-                    res += 1
+        for r in range(rows):
+            for c in range(cols):
+                if grid1[r][c]:
+                    island_cells1.add((r,c))
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid2[r][c] and (r,c) not in visited2:
+                    islands2.append(bfs(r, c))
+       
+        for island in islands2:
+            for cell in island:
+                if cell not in island_cells1:
+                    break
+            else:
+                res += 1
+
         return res
