e6499dbb44ca7e7a9de6f354ca1e653187fed8cb
Michael Senkao <senkaomichael@gmail.com>
2024-08-28 18:51:26 +0530

    Time: 79 ms (5.14%) | Memory: 26.4 MB (5.17%) - LeetSync

--- LeetCode/122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
+++ LeetCode/122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
@@ -1,17 +1,20 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        @cache
+        memo = {}
         def dp(index, hasStock):
             if index >= len(prices):
                 return 0
-
+            if (index, hasStock) in memo:
+                return memo[(index, hasStock)]
             if hasStock:
                 sell = prices[index] + dp(index + 1, False)
                 not_sell = dp(index + 1, True)
-                return max(sell, not_sell)
+                memo[(index, hasStock)] = max(sell, not_sell)
+                return memo[(index, hasStock)]
             else:
                 buy =  dp(index + 1, True) - prices[index]
                 not_buy = dp(index + 1, False)
-                return max(buy, not_buy)
+                memo[(index, hasStock)] = max(buy, not_buy)
+                return memo[(index, hasStock)]
 
         return dp(0, False)
