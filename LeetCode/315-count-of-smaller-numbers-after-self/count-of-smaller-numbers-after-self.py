c7814318b1cc692706d4a2cac0b76c6470607230
Michael Senkao <senkaomichael@gmail.com>
2024-09-06 20:12:42 +0530

    Time: 1723 ms (65.80%) | Memory: 104.1 MB (5.01%) - LeetSync

--- LeetCode/315-count-of-smaller-numbers-after-self/count-of-smaller-numbers-after-self.py
+++ LeetCode/315-count-of-smaller-numbers-after-self/count-of-smaller-numbers-after-self.py
@@ -1,15 +1,45 @@
-from sortedcontainers import SortedList
-
 class Solution:
     def countSmaller(self, nums: List[int]) -> List[int]:
-        
-        sorted_list = SortedList([])
-        res = []
-        n = len(nums)
-        for i in range(n-1, -1, -1):
-            index = bisect_left(sorted_list, nums[i])
-            res.append(index)
-            sorted_list.add(nums[i])
+        def mergeSort(l, r):
+            if l < r:
+                mid = l + (r - l)//2
+                left = mergeSort(l, mid)
+                right = mergeSort(mid + 1, r)
+                return merge(left, right)
+            
+            return [arr[l]]
 
+            
+        def merge(left, right):
+            i = j = 0
+            count = 0
+            merged_arr = []
+    
+            while i < len(left) and j < len(right):
+                
+                if left[i][1] <= right[j][1]:
+                    counts[left[i][0]] += count
+                    merged_arr.append(left[i])
+                    i += 1
+                else:
+                    count += 1
+                    merged_arr.append(right[j])
+                    j += 1
+
+            while i < len(left):
+                counts[left[i][0]] += count
+                merged_arr.append(left[i])
+                i += 1
+            while j < len(right):
+                merged_arr.append(right[j])
+                j += 1
+            return merged_arr
+
+        n = len(nums)
+        arr = [(index, num) for index,num in enumerate(nums)]
+        counts = [0 for _ in range(n)]
+
+        mergeSort(0, n-1)
+
+        return counts
         
-        return reversed(res)
