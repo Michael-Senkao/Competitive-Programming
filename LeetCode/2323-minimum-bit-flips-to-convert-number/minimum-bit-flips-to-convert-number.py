41ab7b9b4340ed9c074618854b9ed781a6d80dd0
Michael Senkao <senkaomichael@gmail.com>
2024-09-11 15:33:22 +0530

    Time: 27 ms (93.90%) | Memory: 16.5 MB (69.15%) - LeetSync

--- LeetCode/2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.py
+++ LeetCode/2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.py
@@ -2,10 +2,8 @@ class Solution:
     def minBitFlips(self, start: int, goal: int) -> int:
         max_ = max(start, goal)
         ans = 0
-        count = 0
         
         while start > 0 or goal > 0:
-            count+= 1
             start_bit = start & 1
             end_bit = goal & 1
             ans += start_bit!=end_bit
