9156aded71425bef7024574b6c71472e283abdcb
Michael Senkao <senkaomichael@gmail.com>
2024-10-11 21:33:36 +0530

    Time: 560 ms (72.07%) | Memory: 22.6 MB (84.98%) - LeetSync

---
+++ LeetCode/2054-the-number-of-the-smallest-unoccupied-chair/the-number-of-the-smallest-unoccupied-chair.py
@@ -0,0 +1,23 @@
+class Solution:
+    def smallestChair(self, times: List[List[int]], targetFriend: int) -> int:
+        n = len(times)
+        indices = [i for i in range(n)]
+
+        indices.sort(key=lambda i: times[i][0])
+        # print(indices)
+        free = [i for i in range(n)]
+        taken = []
+        heapq.heapify(free)
+        heapq.heapify(taken)
+
+        for i in indices:
+            start,end = times[i]
+            while taken and taken[0][0] <= start:
+                _, index = heapq.heappop(taken)
+                heapq.heappush(free, index)
+            if i == targetFriend:
+                return free[0]
+            index = heapq.heappop(free)
+            heapq.heappush(taken, (end, index))
+
+        return -1
