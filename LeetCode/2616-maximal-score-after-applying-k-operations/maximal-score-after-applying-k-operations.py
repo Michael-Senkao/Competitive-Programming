8f8247c85d41ccb56c094ab796df6dba2e1f13c8
Michael Senkao <senkaomichael@gmail.com>
2024-10-14 18:39:09 +0530

    Time: 707 ms (80.06%) | Memory: 31.3 MB (55.06%) - LeetSync

---
+++ LeetCode/2616-maximal-score-after-applying-k-operations/maximal-score-after-applying-k-operations.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxKelements(self, nums: List[int], k: int) -> int:
+        max_heap = [-num for num in nums]
+        heapq.heapify(max_heap)
+
+        result = 0
+
+        for _ in range(k):
+            max_v = -heapq.heappop(max_heap)
+            result += max_v
+            heapq.heappush(max_heap, -ceil(max_v / 3))
+
+        return result
