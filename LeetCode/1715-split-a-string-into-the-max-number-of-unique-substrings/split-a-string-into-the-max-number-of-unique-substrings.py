1d1d29c4912a277405594b2e0bc6be339663198c
Michael Senkao <senkaomichael@gmail.com>
2024-10-21 20:47:44 +0530

    Time: 95 ms (93.72%) | Memory: 16.6 MB (55.61%) - LeetSync

--- LeetCode/1715-split-a-string-into-the-max-number-of-unique-substrings/split-a-string-into-the-max-number-of-unique-substrings.py
+++ LeetCode/1715-split-a-string-into-the-max-number-of-unique-substrings/split-a-string-into-the-max-number-of-unique-substrings.py
@@ -3,14 +3,16 @@ class Solution:
         def helper(index, curr, result: set):
             if index == len(s):
                 if curr and curr not in result:
-                    result.add(curr)
+                    return len(result) + 1
                 return len(result)
 
-            include = helper(index + 1, curr + s[index], result.copy())
+            include = helper(index + 1, curr + s[index], result)
             if not curr or curr in result:
                 return include
+
             result.add(curr)
-            exclude = helper(index + 1, s[index], result.copy())
+            exclude = helper(index + 1, s[index], result)
+            result.remove(curr)
             return max(include, exclude)
 
         return helper(1, s[0], set())
