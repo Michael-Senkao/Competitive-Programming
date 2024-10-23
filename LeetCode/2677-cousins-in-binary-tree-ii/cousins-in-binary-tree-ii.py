236f8e2c144857dd4ed7e2378e461dcc5d5cb387
Michael Senkao <senkaomichael@gmail.com>
2024-10-23 19:53:08 +0530

    Time: 85 ms (100.00%) | Memory: 57.3 MB (83.57%) - LeetSync

--- LeetCode/2677-cousins-in-binary-tree-ii/cousins-in-binary-tree-ii.py
+++ LeetCode/2677-cousins-in-binary-tree-ii/cousins-in-binary-tree-ii.py
@@ -6,43 +6,30 @@
 #         self.right = right
 class Solution:
     def replaceValueInTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        level_sums = []
+        prev_level_sum = root.val
         q = deque()
         q.append(root)
-        
-        # Find level sum using inorder traversal
+
+        # Update the tree values
         while q:
             level_sum = 0
             for _ in range(len(q)):
                 curr_node = q.popleft()
-                level_sum += curr_node.val
-                if curr_node.left:
-                    q.append(curr_node.left)
-
-                if curr_node.right:
-                    q.append(curr_node.right)
-
-            level_sums.append(level_sum)
-
-        # Update the tree values
-        q.append(root)
-        i = 0
-        while q:
-            for _ in range(len(q)):
-                curr_node = q.popleft()
-                curr_node.val = level_sums[i] - curr_node.val
+                curr_node.val = prev_level_sum - curr_node.val
 
                 if curr_node.left:
+                    level_sum += curr_node.left.val
                     if curr_node.right:
+                        level_sum += curr_node.right.val
                         curr_node.left.val += curr_node.right.val
                         curr_node.right.val = curr_node.left.val
                         q.append(curr_node.right)
 
                     q.append(curr_node.left)
                 elif curr_node.right:
+                    level_sum += curr_node.right.val
                     q.append(curr_node.right)
             
-            i += 1
-
+            prev_level_sum = level_sum
             
         return root
