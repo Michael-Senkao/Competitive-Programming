7a046a1faf5f901881f96c711ed6517443e42067
Michael Senkao <senkaomichael@gmail.com>
2024-10-23 20:27:02 +0530

    Time: 3 ms (98.42%) | Memory: 16.8 MB (7.52%) - LeetSync

--- LeetCode/1873-longest-nice-substring/longest-nice-substring.py
+++ LeetCode/1873-longest-nice-substring/longest-nice-substring.py
@@ -1,23 +1,17 @@
 class Solution:
     def longestNiceSubstring(self, s: str) -> str:
-        n = len(s)
-        start = end = -1
+        def long(s):
+            if len(s) < 2:
+                return ""
+            
+            sset = set(s)
+            for i in range(len(s)):
+                ch = s[i]
+                if ch.swapcase() not in s:
+                    s1 = long(s[:i])
+                    s2 = long(s[i+1: ])
+                    return s1 if len(s1) >= len(s2) else s2
+            return s
 
-        for i in range(n):
-            found = set()
-            distinct = 0
-            for j in range(i, n):
-                if s[j].swapcase() in found:
-                    found.add(s[j])
-                elif s[j] not in found:
-                    distinct += 1
-                    found.add(s[j])
+        return long(s)
 
-                if 2*distinct == len(found):
-                    if j - i > end - start:
-                        start,end = i,j
-                
-
-
-        return "" if start == -1 else s[start: end + 1]
-        
