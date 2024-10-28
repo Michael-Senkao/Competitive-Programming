5895093de0a1f227990bcc390613cf188fcf4dc6
Michael Senkao <senkaomichael@gmail.com>
2024-10-28 23:53:24 +0530

    Time: 147 ms (37.84%) | Memory: 41.2 MB (6.67%) - LeetSync

--- LeetCode/2586-longest-square-streak-in-an-array/longest-square-streak-in-an-array.py
+++ LeetCode/2586-longest-square-streak-in-an-array/longest-square-streak-in-an-array.py
@@ -1,23 +1,22 @@
 class Solution:
     def longestSquareStreak(self, nums: List[int]) -> int:
         nums_set = set(nums)
-        set_keys = {num:num for num in nums_set}
-        square_streak = {num: set([num]) for num in nums_set}
-        
-        for num in nums_set:
-            square_root = math.sqrt(num)
-            if square_root*square_root == num and square_root in nums_set:
-                set_key = set_keys[num]
-                square_streak[set_keys[square_root]] = square_streak[set_keys[square_root]] | square_streak[set_key]
-                
-                for x in square_streak[set_key]:
-                    set_keys[x] = set_keys[square_root]
-                del square_streak[set_key]
-
-        # print(square_streak)
-        # print(set_keys)
+        found = defaultdict(int)
         result = 0
-        for key,value in square_streak.items():
-            result = max(result, len(value))
+        for num in nums:
+            curr = num
+            count = 1
+            square = curr * curr
+            while square in nums_set:
+                if square in found:
+                    count += found[square]
+                    break
+                count += 1
+                square *= square
+            result = max(result, count)
+            found[num] = count
+
+
+        
         return result if result > 1 else -1
         
