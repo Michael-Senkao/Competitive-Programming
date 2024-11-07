30c4f108a8ddbdbad9a8bd9bd5859af60ab1a1dc
Michael Senkao <senkaomichael@gmail.com>
2024-11-07 21:33:02 +0530

    Time: 546 ms (45.00%) | Memory: 36.1 MB (5.15%) - LeetSync

--- LeetCode/2356-largest-combination-with-bitwise-and-greater-than-zero/largest-combination-with-bitwise-and-greater-than-zero.py
+++ LeetCode/2356-largest-combination-with-bitwise-and-greater-than-zero/largest-combination-with-bitwise-and-greater-than-zero.py
@@ -1,10 +1,10 @@
 class Solution:
     def largestCombination(self, candidates: List[int]) -> int:
-        max_cols = len(bin(max(candidates))[2:])
-        bits = [(max_cols - len(bin(num)[2:]))*'0' + bin(num)[2:] for num in candidates]
+        max_cols = len(bin(max(candidates)))
+        bits = [(max_cols - len(bin(num)))*'0' + bin(num) for num in candidates]
         result = 0
 
-        for c in range(max_cols):
+        for c in range(2,max_cols):
             temp = 0
             for r in range(len(candidates)):
                 temp += bits[r][c] == '1'
