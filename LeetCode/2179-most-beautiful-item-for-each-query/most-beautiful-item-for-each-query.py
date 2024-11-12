8e98bc1e8059d897a8b0ba2f817d1f29ae2a37e8
Michael Senkao <senkaomichael@gmail.com>
2024-11-12 19:13:18 +0530

    Time: 252 ms (46.34%) | Memory: 65.8 MB (63.44%) - LeetSync

--- LeetCode/2179-most-beautiful-item-for-each-query/most-beautiful-item-for-each-query.py
+++ LeetCode/2179-most-beautiful-item-for-each-query/most-beautiful-item-for-each-query.py
@@ -1,14 +1,23 @@
 class Solution:
+    def search(self,target,max_beauties):
+        left = 0
+        right = len(max_beauties)-1
+        res = 0
+        while left <= right:
+            mid = left + (right-left)//2
+            if max_beauties[mid][0] <= target:
+                res = max_beauties[mid][1]
+                left = mid+1
+            else:
+                right = mid-1
+        return res
     def maximumBeauty(self, items: List[List[int]], queries: List[int]) -> List[int]:
         items.sort()
-        queries = [(num,i) for i,num in enumerate(queries)]
-        queries.sort()
-        res = [0] * len(queries)
-        j = 0
-        max_beauty = 0
-        for i in range(len(queries)):
-            while j < len(items) and items[j][0] <= queries[i][0]:
-                max_beauty = max(max_beauty,items[j][1]) 
-                j+=1
-            res[queries[i][1]] = max_beauty
-        return res
+        max_beauties = []
+        max_beauty = items[0][1]
+        for i in range(1,len(items)):
+            if items[i][0] != items[i-1][0]:
+                max_beauties.append((items[i-1][0],max_beauty))
+            max_beauty = max(max_beauty,items[i][1])
+        max_beauties.append((items[-1][0],max_beauty))
+        return [self.search(i,max_beauties) for i in queries]
