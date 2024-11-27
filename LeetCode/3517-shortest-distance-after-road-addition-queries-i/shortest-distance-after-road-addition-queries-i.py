2ec4bd49ae6fe219e655f6a914c2f32f409fd770
Michael Senkao <senkaomichael@gmail.com>
2024-11-27 23:54:54 +0530

    Time: 92 ms (94.74%) | Memory: 18.4 MB (8.31%) - LeetSync

---
+++ LeetCode/3517-shortest-distance-after-road-addition-queries-i/shortest-distance-after-road-addition-queries-i.py
@@ -0,0 +1,23 @@
+class Solution:
+    def shortestDistanceAfterQueries(self, n: int, queries: List[List[int]]) -> List[int]:
+        def update(node):
+            for next_node in graph[node]:
+                if distance[node] + 1 < distance[next_node]:
+                    distance[next_node] = distance[node] + 1
+                    update(next_node)
+
+
+        graph = defaultdict(list)
+        distance = [0]
+        res = []
+        for i in range(1,n):
+            distance.append(i)
+            graph[i-1].append(i)
+        
+        for u,v in queries:
+            graph[u].append(v)
+            update(u)
+            res.append(distance[-1])
+
+        print(res)
+        return res
