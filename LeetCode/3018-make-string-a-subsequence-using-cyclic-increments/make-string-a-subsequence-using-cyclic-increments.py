38ff97b8df701f748b037d0d6d3e0f06a3dcd227
Michael Senkao <senkaomichael@gmail.com>
2024-12-04 23:29:53 +0530

    Time: 67 ms (80.56%) | Memory: 17.6 MB (50.24%) - LeetSync

--- LeetCode/3018-make-string-a-subsequence-using-cyclic-increments/make-string-a-subsequence-using-cyclic-increments.py
+++ LeetCode/3018-make-string-a-subsequence-using-cyclic-increments/make-string-a-subsequence-using-cyclic-increments.py
@@ -6,14 +6,14 @@ class Solution:
         if n1 < n2:
             return False
 
-        j = 0
-        for i in range(n1):
+        i, j = 0, 0
+        while i < n1 and j < n2:
             pos1, pos2  = ord(str1[i]) - 97, ord(str2[j]) - 97
             if pos1 == pos2 or (pos1 + 1) % 26 == pos2:
                 j += 1
-                if j == n2:
-                    return True
+
+            i += 1
 
 
-        return False
+        return j == n2
         
