fb95eae9b77c8980800954c5846a3af24eb9ce55
Michael Senkao <senkaomichael@gmail.com>
2024-12-12 21:43:59 +0530

    Time: 3 ms (87.72%) | Memory: 17.4 MB (13.92%) - LeetSync

---
+++ LeetCode/2692-take-gifts-from-the-richest-pile/take-gifts-from-the-richest-pile.py
@@ -0,0 +1,11 @@
+class Solution:
+    def pickGifts(self, gifts: List[int], k: int) -> int:
+        max_heap = [-gift for gift in gifts]
+        heapq.heapify(max_heap)
+
+        while k:
+            largest_gift = -heapq.heappop(max_heap)
+            heapq.heappush(max_heap, -int(math.sqrt(largest_gift)))
+            k -= 1
+
+        return -sum(max_heap)
