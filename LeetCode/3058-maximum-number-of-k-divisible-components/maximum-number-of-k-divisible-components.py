306c3f33b071bb572de6eb5e9c63037ccca65326
Michael Senkao <senkaomichael@gmail.com>
2024-12-21 23:03:57 +0530

    Time: 179 ms (48.78%) | Memory: 32.1 MB (85.37%) - LeetSync

--- LeetCode/3058-maximum-number-of-k-divisible-components/maximum-number-of-k-divisible-components.py
+++ LeetCode/3058-maximum-number-of-k-divisible-components/maximum-number-of-k-divisible-components.py
@@ -1,27 +1,22 @@
 class Solution:
     def maxKDivisibleComponents(self, n: int, edges: List[List[int]], values: List[int], k: int) -> int:
-        if n==1:
+        if n == 1:
             return 1
+        graph = defaultdict(list)
+        degree = [0]*n
 
-        degree = [0]*n # To track the degree of each node
-        graph = defaultdict(list) # Adjacency list for tree
+        for a,b in edges:
+            graph[a].append(b)
+            graph[b].append(a)
+            degree[a] += 1
+            degree[b] += 1
 
-        # Create adjacency list for the tree
-        for u,v in edges:
-            graph[u].append(v)
-            graph[v].append(u)
-            degree[u] += 1
-            degree[v] += 1
-        
         q = deque()
         count = 0
-
-        # Initialize the queue with the leaf nodes
         for i in range(n):
             if degree[i] == 1:
                 q.append(i)
 
-        # Use BFS to traverse the tree
         while q:
             node = q.popleft()
             if values[node] % k == 0:
@@ -34,6 +29,6 @@ class Solution:
                 degree[nei] -= 1
                 if degree[nei] == 1:
                     q.append(nei)
-                
-        
+
+
         return count
