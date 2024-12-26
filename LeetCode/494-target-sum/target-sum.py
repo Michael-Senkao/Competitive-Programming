9966c69df54e8eebfb388427e44cd6adc54f21be
Michael Senkao <senkaomichael@gmail.com>
2024-12-26 23:10:33 +0530

    Time: 138 ms (71.89%) | Memory: 45.2 MB (5.57%) - LeetSync

--- LeetCode/494-target-sum/target-sum.py
+++ LeetCode/494-target-sum/target-sum.py
@@ -1,21 +1,21 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
 
-        memo = {}
-
+        # memo = {}
+        @cache
         def dp(index, curr_sum):
             if index == n:
                 if curr_sum == target:
                     return 1
                 return 0
-            if (index, curr_sum) in memo:
-                return memo[(index, curr_sum)]
+            # if (index, curr_sum) in memo:
+            #     return memo[(index, curr_sum)]
 
             sub = dp(index + 1, -nums[index] + curr_sum)
             add = dp(index + 1, nums[index] + curr_sum)
 
-            memo[(index, curr_sum)] = add + sub
-            return memo[(index, curr_sum)]
+            # memo[(index, curr_sum)] = add + sub
+            return add + sub # memo[(index, curr_sum)]
 
         n = len(nums)
         
