cb4fa09b3940fb6acad1d268a6099d178a7a4e78
Michael Senkao <senkaomichael@gmail.com>
2024-12-26 22:50:07 +0530

    Time: 212 ms (14.37%) | Memory: 38.6 MB (17.20%) - LeetSync

--- LeetCode/494-target-sum/target-sum.py
+++ LeetCode/494-target-sum/target-sum.py
@@ -1,22 +1,24 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
-        # memo = {}
-        @cache
-        def dp(index, curr_value):
-            if index == len(nums):
-                return 1 if curr_value == target else 0
-            
-            # Check if the result is already computed
-            # if (index, curr_value) in memo:
-            #     return memo[(index, curr_value)]
-            
-            # Calculate the number of ways by adding and subtracting the current number
-            add = dp(index + 1, curr_value + nums[index])
-            subtract = dp(index + 1, curr_value - nums[index])
-            
-            # Store the result in the memo dictionary
-            # memo[(index, curr_value)] = add + subtract
-            # return memo[(index, curr_value)]
-            return add + subtract
+
+        memo = {}
+
+        def dp(index, curr_sum):
+            if index == n:
+                if curr_sum == target:
+                    return 1
+                return 0
+            if (index, curr_sum) in memo:
+                return memo[(index, curr_sum)]
+
+            sub = dp(index + 1, -nums[index] + curr_sum)
+            add = dp(index + 1, nums[index] + curr_sum)
+
+            memo[(index, curr_sum)] = add + sub
+            return memo[(index, curr_sum)]
+
+        n = len(nums)
         
-        return dp(0, 0)
+        return dp(0,0)
+
+
