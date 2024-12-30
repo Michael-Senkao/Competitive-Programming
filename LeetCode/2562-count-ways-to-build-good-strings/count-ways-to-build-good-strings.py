b6dc1a1923c23bf20a3ed2e8f8e99fc064ccff06
Michael Senkao <senkaomichael@gmail.com>
2024-12-30 21:47:14 +0530

    Time: 157 ms (55.22%) | Memory: 48.7 MB (56.17%) - LeetSync

---
+++ LeetCode/2562-count-ways-to-build-good-strings/count-ways-to-build-good-strings.py
@@ -0,0 +1,22 @@
+class Solution:
+    def countGoodStrings(self, low: int, high: int, zero: int, one: int) -> int:
+
+        def backtrack(length):
+            if length > high:
+                return 0
+            
+            if dp[length] != -1:
+                return dp[length]
+            add_zero = backtrack(length + zero) % MOD
+            add_one = backtrack(length + one) % MOD
+
+            dp[length] = (add_zero + add_one + (low <= length <= high)) % MOD
+
+
+            return dp[length]
+
+        MOD = 10**9 + 7
+        dp = [-1]*(high + 1)
+        return backtrack(0)
+            
+        
