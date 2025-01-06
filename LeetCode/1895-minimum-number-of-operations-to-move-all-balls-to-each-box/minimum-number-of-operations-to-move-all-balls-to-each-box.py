c468625593b4af9ae4caac4664d7e6f1534fcddc
Michael Senkao <senkaomichael@gmail.com>
2025-01-06 18:55:13 +0530

    Time: 10 ms (70.68%) | Memory: 17.8 MB (46.76%) - LeetSync

--- LeetCode/1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.py
+++ LeetCode/1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.py
@@ -3,22 +3,22 @@ class Solution:
         n = len(boxes)
         answer = [0] * n
 
-        balls_to_left = 0
+        balls_to_left = int(boxes[0])
         moves_to_left = 0
-        balls_to_right = 0
+        balls_to_right = int(boxes[-1])
         moves_to_right = 0
 
         # Single pass: calculate moves from both left and right
-        for i in range(n):
+        for i in range(1,n):
             # Left pass
-            answer[i] += moves_to_left
-            balls_to_left += int(boxes[i])
             moves_to_left += balls_to_left
+            balls_to_left += int(boxes[i])
+            answer[i] += moves_to_left
 
             # Right pass
             j = n - 1 - i
-            answer[j] += moves_to_right
-            balls_to_right += int(boxes[j])
             moves_to_right += balls_to_right
+            balls_to_right += int(boxes[j])
+            answer[j] += moves_to_right
 
         return answer
