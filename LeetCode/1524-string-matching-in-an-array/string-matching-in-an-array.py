af26f0f8a3404ce071f8b4563d5e649294e31982
Michael Senkao <senkaomichael@gmail.com>
2025-01-07 19:19:36 +0530

    Time: 3 ms (64.01%) | Memory: 17.9 MB (12.59%) - LeetSync

--- LeetCode/1524-string-matching-in-an-array/string-matching-in-an-array.py
+++ LeetCode/1524-string-matching-in-an-array/string-matching-in-an-array.py
@@ -1,23 +1,11 @@
 class Solution:
     def stringMatching(self, words: List[str]) -> List[str]:
-
-        def findSubstrings(word):
-            m = len(word)
-
-            for i in range(m):
-                curr = ''
-                for j in range(i, m):
-                    curr += word[j]
-                    if curr in words_set and len(curr) < m:
-                        ans.append(curr)
-                        words_set.remove(curr)
-        
-
-        n = len(words)
         ans = []
-        words_set = set(words)
-        for i in range(n):
-            if words[i] in words_set:
-                findSubstrings(words[i])
-
+        for i in range(len(words)):
+            for j in range(len(words)):
+                if i == j or len(words[i]) > len(words[j]):
+                    continue
+                if words[i] in words[j]:
+                    ans.append(words[i])
+                    break
         return ans
