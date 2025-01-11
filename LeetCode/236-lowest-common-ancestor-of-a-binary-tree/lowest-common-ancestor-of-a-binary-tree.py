038ab94af85ef1a353f2c20997f9a4101c71ea42
Michael Senkao <senkaomichael@gmail.com>
2025-01-11 19:01:22 +0530

    Time: 54 ms (45.89%) | Memory: 22.1 MB (18.97%) - LeetSync

--- LeetCode/236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.py
+++ LeetCode/236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.py
@@ -6,29 +6,14 @@
 #         self.right = None
 
 class Solution:
-    def dfs(self, node, target, path):
-        if node is None:
-            return False
-        if node == target:
-            path.append(node)
-            return True
-        if self.dfs(node.left, target, path) or self.dfs(node.right, target, path):
-            path.append(node)
-            
-            return True
-        return False
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        path1 = []
-        path2 = []
-        self.dfs(root, p, path1)
-        self.dfs(root, q, path2)
-        # print(path1, path2)
+        if root is None or root==p or root==q:
+            return root
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p,q)
 
-        i = len(path1) - 1
-        j = len(path2) - 1
-        while i >= 0 and j >= 0 and path1[i] == path2[j]:
-            i -= 1
-            j -= 1
-        
-            
-        return path1[i+1]
+        if right is None:
+            return left
+        if left is None:
+            return right
+        return root
