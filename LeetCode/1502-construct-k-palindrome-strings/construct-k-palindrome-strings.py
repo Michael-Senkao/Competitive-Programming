4800a0b19735d3f7bb7c3077ee8170272ebcd814
Michael Senkao <senkaomichael@gmail.com>
2025-01-12 11:29:00 +0530

    Time: 43 ms (41.94%) | Memory: 18 MB (52.69%) - LeetSync

--- LeetCode/1502-construct-k-palindrome-strings/construct-k-palindrome-strings.py
+++ LeetCode/1502-construct-k-palindrome-strings/construct-k-palindrome-strings.py
@@ -1,12 +1,11 @@
 class Solution:
     def canConstruct(self, s: str, k: int) -> bool:
-        n = len(s)
-        if n < k:
+        if len(s) < k:
             return False
-        if n == k:
-            return True
-        count = Counter(s)
-        oddCount = 0
-        for val in count.values():
-            oddCount += val&1
-        return oddCount <= k
+        freq = [0]*26
+
+        for ch in s:
+            freq[ord(ch) - 97] ^= 1
+
+       
+        return freq.count(1) <= k
