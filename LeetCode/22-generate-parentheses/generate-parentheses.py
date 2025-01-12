474dfcded2300574634f15724f45b69c70c060f2
Michael Senkao <senkaomichael@gmail.com>
2025-01-12 20:50:46 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (31.08%) - LeetSync

--- LeetCode/22-generate-parentheses/generate-parentheses.py
+++ LeetCode/22-generate-parentheses/generate-parentheses.py
@@ -1,15 +1,16 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        def helper(curr, opened_par, closed_par):
-            if opened_par == 0:
-                curr += ")"*closed_par
-                res.append(curr)
-            else:
-                helper(curr + "(", opened_par - 1, closed_par)
-                if closed_par > opened_par:
-                    helper(curr + ")", opened_par, closed_par - 1)
-
         res = []
-        helper("(", n-1, n)
-        print(res)
+        def backtrack(open,close,string):
+            if open == 0 and close == 0:
+                res.append(string)
+                return
+            if open == close:
+                backtrack(open-1,close,string+'(')
+            elif open:
+                backtrack(open-1,close,string+'(')
+                backtrack(open,close-1,string+')')
+            elif close:
+                backtrack(open,close-1,string+')')
+        backtrack(n,n,'')
         return res
