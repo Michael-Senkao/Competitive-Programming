8f8d5500d6e360fd3e59bc7072dac45c9f87f53f
Michael Senkao <senkaomichael@gmail.com>
2025-01-20 23:30:36 +0530

    Time: 111 ms (79.49%) | Memory: 52.1 MB (43.22%) - LeetSync

---
+++ LeetCode/2685-first-completely-painted-row-or-column/first-completely-painted-row-or-column.py
@@ -0,0 +1,21 @@
+class Solution:
+    def firstCompleteIndex(self, arr: List[int], mat: List[List[int]]) -> int:
+        rows,cols = len(mat),len(mat[0])
+
+        cols_count = [rows]*cols
+        rows_count = [cols]*rows
+        positions = {}
+
+        for r in range(rows):
+            for c in range(cols):
+                positions[mat[r][c]] = (r,c)
+
+        for i,val in enumerate(arr):
+            r,c = positions[val]
+            rows_count[r] -= 1
+            cols_count[c] -= 1
+
+            if rows_count[r] == 0 or cols_count[c] == 0:
+                return i
+
+        return -1
