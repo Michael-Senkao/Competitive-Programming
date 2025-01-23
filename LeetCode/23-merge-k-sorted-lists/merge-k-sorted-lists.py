b5d0be1d2cf219a388d8aa6872669c2bcd48133d
Michael Senkao <senkaomichael@gmail.com>
2025-01-23 13:56:25 +0530

    Time: 8 ms (79.68%) | Memory: 20.4 MB (14.60%) - LeetSync

--- LeetCode/23-merge-k-sorted-lists/merge-k-sorted-lists.py
+++ LeetCode/23-merge-k-sorted-lists/merge-k-sorted-lists.py
@@ -3,38 +3,23 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution:
-    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        def mergeSort(left, right):
-            if left < right:
-                mid = left + (right - left)//2
-                l = mergeSort(left, mid)
-                r = mergeSort(mid+1, right)
-                return merge(l, r)
 
-            return lists[left]
-        
-        def merge(left, right):
-            dummy = curr = ListNode()
-            while left and right:
-                if left.val < right.val:
-                    curr.next = left
-                    left = left.next
-                else:
-                    curr.next = right
-                    right = right.next
-                curr = curr.next
-            if left:
-                curr.next = left
-            if right:
-                curr.next = right
-            # print_(dummy.next)
-            return dummy.next
-        # def print_(start):
-        #     while start:
-        #         print(start.val, end="->")
-        #         start = start.next
-        #     print()
-        if not lists: return None
-        return mergeSort(0, len(lists) - 1)
-        
+class Solution:    
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        min_heap = []
+        for index, node in enumerate(lists):
+            if node:
+                heapq.heappush(min_heap, (node.val, index))
+
+
+        dummy = tail = ListNode()
+        while min_heap:
+            val,index = heapq.heappop(min_heap)
+            tail.next = ListNode(val)
+            tail = tail.next
+
+            lists[index] = lists[index].next
+            if lists[index]:
+                heapq.heappush(min_heap, (lists[index].val, index))
+
+        return dummy.next
